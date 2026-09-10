2493b17582e309b3ad0c1aeaa24c3b7cd15390a3
rodorodriguez2733-ship-it <rodorodriguez2733@gmail.com>
2026-09-10 15:07:24 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-const CACHE='battery-hub-v1';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.mode==='navigate')e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')))})
+const CACHE='battery-repair-pos-store-v2';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));
+  }
+});
